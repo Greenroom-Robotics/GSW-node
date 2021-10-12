2f3addb76387caac1cf400c8d3201f500941064a
Zac Pullen <zac@greenroomrobotics.com>
2021-10-12 09:54:19 +0800

    update cmake to use shared lib

--- TeosCpp/teos-cpp.cpp
+++ TeosCpp/teos-cpp.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "TeosSea.h"
 
+// Some basic code to import and run a few examples of the TeosCpp library. 
 
 int main()
 {
     std::cout << "Testing teos-cpp funcitons" << std::endl;
+
+    auto teos_sea = TeosSea();
+
+    auto value = teos_sea.gsw_c_from_sp(1.21, 12.12, 98.22);
+    std::cout << "Answer: " << value << std::endl;
 }
