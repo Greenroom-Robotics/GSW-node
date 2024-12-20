e99a16a3b17f13f15357d69de44a4761267f7eaf
Blake Rogan <blake.rogan@greenroomrobotics.com>
2024-12-20 11:11:23 +1100

    feat(gsw)!: update release process

--- release.config.cjs
+++ release.config.cjs
@@ -3,22 +3,38 @@ module.exports = {
     plugins: [
         [
             '@semantic-release/commit-analyzer',
-            { preset: 'conventionalcommits' }
+            {preset: 'conventionalcommits'}
         ],
         [
             '@semantic-release/release-notes-generator',
-            { preset: 'conventionalcommits' },
+            {preset: 'conventionalcommits'},
         ],
         '@semantic-release/changelog',
         [
             '@semantic-release/npm',
-            { pkgRoot: 'packages/gsw_cpp' }
+            {pkgRoot: 'packages/gsw_cpp'}
         ],
         [
             '@semantic-release/npm',
-            { pkgRoot: 'packages/gsw_node' }
+            {pkgRoot: 'packages/gsw_node'}
         ],
-        '@semantic-release/github',
+        [
+            '@semantic-release/exec',
+            {execCwd: 'packages/gsw_cpp', publishCmd: 'pnpm pack'}
+        ],
+        [
+            '@semantic-release/exec',
+            {execCwd: 'packages/gsw_node', publishCmd: 'pnpm pack'}
+        ],
+        [
+            '@semantic-release/github',
+            {
+                assets: [
+                    'packages/gsw_cpp/greenroom-robotics-gsw_cpp-*.tgz',
+                    'packages/gsw_node/greenroom-robotics-gsw_node-*.tgz'
+                ]
+            }
+        ],
         '@semantic-release/git',
     ],
 };
